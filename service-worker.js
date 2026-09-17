6e820519729bbbb6a465aca062c5afc1c03e22a4
Ariel via Manus <noreply@users.noreply.github.com>
2026-09-17 21:11:54 +0000

    Publish Skyline Signal PWA update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'skyline-signal-1789679223520';
+const CACHE = 'skyline-signal-1789679494083';
 const APP_SHELL = [
   '/', '/manifest.json',
   '/scenery/airport.jpg', '/scenery/crosswind-coast.jpg', '/scenery/peak-rush-hour.jpg', '/scenery/superstorm-radar.jpg',
